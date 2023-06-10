2342b6ddc58ab33cd80678d9526e593c19dc3db1
MarkoTechArt <marko.stevanovic99@gmail.com>
2023-06-10 15:34:19 +0200

    Miasma

--- Assets/Miasma.cs
+++ Assets/Miasma.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Miasma : MonoBehaviour
 {
-    
+    private ParticleSystemRenderer _particle;
+    private float duration = 2f;
     void Start()
     {
-        
+        _particle = GetComponent<ParticleSystemRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+    void Dissolve()
+    {
+        _particle.material.SetFloat("DissolveValue", 0);
+    }
+    IEnumerator ChangeValueOverTime()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            //float currentValue = Mathf.Lerp(startValue, targetValue, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        yield return null;
+    }
 }
