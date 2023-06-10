175e7ceaa3b84f2aec5826a7a6acb10b8844c360
miljkom <71881202+miljkom@users.noreply.github.com>
2023-06-10 16:42:13 +0200

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,11 +7,4 @@ public class Player : MonoBehaviour
     public float battery;
     public float corrosion;
     public float water;
-
-    public Player()
-    {
-        this.battery = 100f;
-        this.corrosion = 100f;
-        this.water = 100f;
-    }
 }
