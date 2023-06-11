5cb1732cbd97f16ebc0a293fcde47593905a1dd5
miljkom <71881202+miljkom@users.noreply.github.com>
2023-06-11 02:04:07 +0200

    push

--- Assets/Scripts/InteractionPromptUI.cs
+++ Assets/Scripts/InteractionPromptUI.cs
@@ -8,17 +8,20 @@ using UnityEngine.UI;
 public class InteractionPromptUI : MonoBehaviour
 {
     [SerializeField] private GameObject uiPanel;
+    [SerializeField] private GameObject framePanel;
 
     public bool isDisplayed = false;
     private void Start()
     {
         uiPanel.SetActive(false);
+        framePanel.SetActive(false);
         isDisplayed = false;
     }
 
     public void SetUp(Sprite spriteIcon)
     {
         uiPanel.GetComponent<Image>().sprite = spriteIcon;
+        framePanel.SetActive(true);
         uiPanel.SetActive(true);
         isDisplayed = true;
     }
@@ -26,6 +29,7 @@ public class InteractionPromptUI : MonoBehaviour
     public void Close()
     {
         uiPanel.SetActive(false);
+        framePanel.SetActive(false);
         isDisplayed = false;
     }
 }
